e6d07512ba78119bf2aa0c6d86e3882beace10ca
CauveryRaja <cauveryraja25@gmail.com>
2021-08-15 08:57:22 +0530

    Render List elements without key

--- src/lists_keys/List.js
+++ src/lists_keys/List.js
@@ -18,16 +18,33 @@ class List extends React.Component {
         items.splice(index);
         this.setState({
             list: items
-        })
+        });
+    }
+
+    clickListener2(index) {
+        let items = [...this.state.list];
+        items.splice(index);
+        // items.pop();
+        // items[index].label = 'updated...';
+        this.setState({
+            list: items
+        });
     }
 
     render() {
-        return this.state.list.map((item, index) => {
-           return <>
-            <li>{item.label}</li>
-            <button onClick={this.clickListener.bind(this, index)}>x</button>
-           </>
-        });
+        // return this.state.list.map((item, index) => {
+        //    return <>
+        //     <li>{item.label}</li>
+        //     <button onClick={this.clickListener.bind(this, index)}>x</button>
+        //    </>
+        // });
+        return <ul>
+            <li>{this.state.list[0].label}</li>
+            <li>{this.state.list[1].label}</li>
+            <li>{this.state.list[2].label}</li>
+            <li>{this.state.list[3].label}</li>
+            <button onClick={this.clickListener2.bind(this, 2)}>x</button>
+        </ul>
     }
 }
 
