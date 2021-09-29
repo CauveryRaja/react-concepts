0f68abbc7972a68119ddf2cef37ab34f59aa4029
CauveryRaja <cauveryraja25@gmail.com>
2021-09-29 20:47:39 +0530

    Analyse unnecessary child component rerendering

---
+++ src/rerender/Sep29/Home.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react/cjs/react.development';
+
+function Dashboard({ count }) {
+    return <p>Count: {count}</p>;
+}
+
+function Header() {
+    console.log('Rendering Header...');
+    return <p>Static header</p>;
+}
+
+function Home () {
+    let [count, setCount] = useState(0);
+
+    console.log('Rerendering...', count);
+
+    useEffect(() => {
+        setInterval(() => setCount(count++), 2000);
+    }, []);
+
+    return <h2>Welcome to check Rerendering !
+        <Header />
+        <Dashboard count={count}/>
+    </h2>
+};
+
+export default Home;
