dfab103154ebcba4ca68980465198dc0ae3fc3dd
CauveryRaja <cauveryraja25@gmail.com>
2021-12-30 20:40:32 +0530

    Understand state update, rerender and execution of side effects

---
+++ src/side-effects/Home.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+const Home = () => {
+    const [count, setCount] = useState(0);
+    const [message, setMessage] = useState();
+    console.log('Outside effects', count);
+
+    useEffect(() => {
+        console.log('Count updated ', count);
+    }, [count])
+
+    const clickListener = () => {
+        console.log('Button clicked');
+        setCount(count+1);
+        setCount(count+2);
+        setMessage('Hello World !');
+        console.log('Button : After setting count', count);
+    };
+
+    useEffect(() => {
+        console.log('Effect : Before setting');
+        setCount(count+1);
+        setCount(count+2);
+        setMessage('Hello World !');
+        console.log('Effect : After setting', count);
+    }, []);
+
+    return <>
+        <button onClick={clickListener}>Step up</button>
+    </>
+};
+
+export default Home;
