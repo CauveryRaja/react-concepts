d9752b258067354980b8634e208a2955738aa848
CauveryRaja <cauveryraja25@gmail.com>
2022-01-08 20:49:11 +0530

    Check if all elements are created again while rerender

--- src/rerender/Jan05/Home.js
+++ src/rerender/Jan05/Home.js
@@ -4,10 +4,24 @@ const Home = () => {
     let [count, setCount] = useState(0);
     let message = 'Count is ' + count;
 
-    return <>
-        <p>{message}</p>
-        <button onClick={() => setCount(count+1)}>Increment</button>
-    </>
+    const clickListener = () => {
+        setCount((previousCount) => {
+            console.log('Updating count ', previousCount);
+            return count+1;
+        });
+    };
+
+    return <div>
+        {console.log('Inside div')}
+        <p>
+            {console.log('Inside para')}
+            {message}
+        </p>
+        <button onClick={clickListener}>
+            {console.log('Inside button')}
+            Increment
+        </button>
+    </div>
 };
 
 export default Home;
